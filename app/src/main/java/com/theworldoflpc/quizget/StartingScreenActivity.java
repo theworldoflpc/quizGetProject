4f4292a89d52e44bac25fcff1dd07e59c743c2d6
theworldoflpc <63312930+theworldoflpc@users.noreply.github.com>
2020-04-16 10:31:26 -0400

    First commit

---
+++ app/src/main/java/com/theworldoflpc/quizget/StartingScreenActivity.java
@@ -0,0 +1,15 @@
+package com.theworldoflpc.quizget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartingScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // test
+    }
+}
